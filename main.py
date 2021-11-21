7fcce293f7f7e780433a6727a3be83314db3d0a9
Agent502 <77380511+agent502@users.noreply.github.com>
2021-11-21 10:28:25 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pywhatkit as pw
+
+text = ''' ''' #text which you want to convert in handwriting
+
+pw.text_to_handwriting(text, 'file.png') #there is a lot of parameters you can use them in this code
+print('Done without any errors')
+
+
+## more information at https://pypi.org/project/pywhatkit/ ##
